d43cef46f3ce5a9149879914c883d3733a565fc8
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-26 10:01:43 -0400

    Create operator function

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function divideTwoNumbers(a, b) {
     a = +a, b = +b;
     return a / b;
 }
+
+function operator(number1, number2, operator) {
+    if (operator === "+") {
+        addTwoNumbers(number1, number2);
+    } else if (operator === "-") {
+        subtractTwoNumbers(number1, number2);
+    } else if (operator === "*") {
+        multiplyTwoNumbers(number1, number2);
+    } else if (operator === "/") {
+        divideTwoNumbers(number1, number2);
+    } 
+}
